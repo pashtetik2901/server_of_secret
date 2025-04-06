0632c01f09317bf05797e9eff48d2028dc9e46bf
Паша Гуров <pashagurov@MacBook-Air-Pasa.local>
2025-04-06 18:23:37 +0300

    second

--- init.sql
+++ init.sql
@@ -3,7 +3,7 @@
 
 CREATE TABLE IF NOT EXISTS secret(
     id SERIAL PRIMARY KEY,
-    secret_key VARCHAR(512) UNIQUE NOT NULL,
+    secret_key VARCHAR(512) NOT NULL,
     secret_value VARCHAR(1024),
     passphrase VARCHAR(255),
     ttl_seconds INTEGER
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS secret(
 
 CREATE TABLE IF NOT EXISTS logger(
     id SERIAL PRIMARY KEY,
-    secret_key VARCHAR(512) UNIQUE NOT NULL,
+    secret_key VARCHAR(512) NOT NULL,
     action_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     action VARCHAR(255),
     ip_address VARCHAR(255)
